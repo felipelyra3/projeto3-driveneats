478c8f7bef95a58f925291a5abd8c39684fb98ca
felipelyra3 <felipelyra3@gmail.com>
2022-06-10 02:22:43 -0300

    2022-06-08_SelecionarElementoAlphaVersion

---
+++ script.js
@@ -0,0 +1,7 @@
+function esconderSelecao() {
+    let esconderBorda = document.querySelector(".bloco");
+    esconderBorda.classList.toggle("bordaVerde");
+    let esconderIconeVerificado = document.querySelector(".iconeVerificado");
+    esconderIconeVerificado.classList.toggle("esconder");
+}
+
